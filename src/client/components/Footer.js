b6ea2bb82c5030d5a130b271c65713e14ec34bc0
g4ntavya <k13rohilla@gmail.com>
2025-06-05 09:53:56 +0530

    hyperlink added

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -23,10 +23,10 @@ const Footer = () => {
         <div className="footer-section">
           <h3>Connect With Us</h3>
           <div className="social-links">
-            <a href="#"><i className="fab fa-facebook-f"></i></a>
-            <a href="#"><i className="fab fa-twitter"></i></a>
-            <a href="#"><i className="fab fa-instagram"></i></a>
-            <a href="#"><i className="fab fa-pinterest"></i></a>
+            <a href="https://www.facebook.com/odoo"><i className="fab fa-facebook-f"></i></a>
+            <a href="https://x.com/Odoo"><i className="fab fa-twitter"></i></a>
+            <a href="https://www.instagram.com/odoo.official/"><i className="fab fa-instagram"></i></a>
+            <a href="https://github.com/odoo/odoo"><i className="fab fa-github"></i></a>
           </div>
         </div>
       </div>
